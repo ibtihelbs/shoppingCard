24fad35d87ca94a98e5d3e9472347d525517b26d
ibtihelbs <ibtihel.bensalah@outlook.fr>
2023-02-12 15:36:06 +0100

    index

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 const main = document.querySelector('main');
-
+let cardItems = [
+    {
+       title: 'pink pair of sneakers',
+       img: './images/img1.avif',
+       price: 40,
+       quantity: 2,
+       size: 's',
+       color: "pink",
+       loved: true,
+    },
+    {
+       title: 'orange pair of sneakers',
+       img: './images/img2.avif',
+       price: 45,
+       quantity: 3,
+       size: 'Xl',
+       color: "orange",
+       loved: false,
+    },
+    {
+       title: 'orange pair of sneakers',
+       img: './images/img3.jpg',
+       price: 50,
+       quantity: 4,
+       size: 'L',
+       color: "orange",
+       loved: false,
+    },
+  ]
   const innerHtml = (data) => {
     return `<div class="single-card">
   <div>
