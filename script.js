5c051782e97a6ccc85af8242e75ddd1731ba4b91
ibtihelbs <ibtihel.bensalah@outlook.fr>
2023-02-12 11:09:45 +0100

    second

---
+++ script.js
@@ -0,0 +1,112 @@
+const main = document.querySelector('main');
+let cardItems = [
+    {
+       title: 'pink pair of sneakers',
+       img: './images/img1.avif',
+       price: 40,
+       quantity: 2,
+       size: 's',
+       color: "pink",
+       loved: true,
+    },
+    {
+       title: 'orange pair of sneakers',
+       img: './images/img2.avif',
+       price: 45,
+       quantity: 3,
+       size: 'Xl',
+       color: "orange",
+       loved: false,
+    },
+    {
+       title: 'orange pair of sneakers',
+       img: './images/img3.jpg',
+       price: 50,
+       quantity: 4,
+       size: 'L',
+       color: "orange",
+       loved: false,
+    },
+  ]
+  const innerHtml = (data) => {
+    return `<div>
+  <div>
+      <img src=${data.img} alt=${data.title}>
+      <div class="quantity-handler">
+       <button class="dec">-</button>
+       <input type="text" value=${data.quantity}>
+       <button class="inc">+</button>
+      </div>
+  </div>
+  <div>
+      <h1>Pink pair of sneakers</h1>
+      <div class="info">
+          <div>
+              <h3>price</h3>
+              <span>${data.price}$</span>
+          </div>
+          <div>
+              <h3>Sub total</h3>
+              <span>${ data.price * data.quantity }$</span>
+          </div>
+          <div>
+              <h3>size</h3>
+              <span>${data.size}</span>
+          </div>
+          <div>
+              <h3>choosen color</h3>
+              <span>pink</span>
+          </div>
+
+      </div>
+  </div>
+  <div class="manage-btn col">
+      <button class="heart ${data.loved ? 'loved': ''}">
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-heart" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+              <path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572"></path>
+           </svg>
+      </button>
+      <button class="delete">
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+              <path d="M4 7l16 0"></path>
+              <path d="M10 11l0 6"></path>
+              <path d="M14 11l0 6"></path>
+              <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12"></path>
+              <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3"></path>
+           </svg>
+      </button>
+  </div>
+</div>`};
+
+function render(){
+    cardItems.forEach(function(item){
+        main.innerHTML += innerHtml(item)
+     })
+}
+render();
+const hearts = document.querySelectorAll('.heart');
+console.log(hearts)
+
+hearts.forEach(function(heart, index){
+    heart.addEventListener('click', function(e){
+        e.preventDefault();
+        cardItems[index].loved = !cardItems[index].loved;
+        console.log(cardItems)
+        main.innerHTML='';
+        render();
+    })
+})
+
+
+const deleteCards = document.querySelectorAll('.delete');
+deleteCards.forEach(function(item, index){
+    item.addEventListener('click', function(e){
+        e.preventDefault();
+        const filtered = cardItems.filter((card, i) => i !== index)
+        main.innerHTML='';
+        cardItems = filtered;
+        render();
+    })
+})
